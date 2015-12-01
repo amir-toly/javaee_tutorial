f2bd254d67fe26fe13500acdda440a3c6f2c73e9
Amir TOLY <amir.toly@gmail.com>
2015-12-01 19:14:41 +0100

    Display content from a JSP

---
+++ WebContent/WEB-INF/test.jsp
@@ -0,0 +1,12 @@
+<%@ page pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Test</title>
+	</head>
+	
+	<body>
+		<p>This is a page generated from a JSP.</p>
+	</body>
+</html>
--- src/com/sdzee/servlets/Test.java
+++ src/com/sdzee/servlets/Test.java
@@ -1,7 +1,6 @@
 package com.sdzee.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,21 +14,7 @@ public class Test extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		resp.setContentType("text/html");
-		resp.setCharacterEncoding("UTF-8");
-		
-		PrintWriter out = resp.getWriter();
-		
-		out.println("<!DOCTYPE html>");
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<meta charset=\"utf-8\" />");
-		out.println("<title>Test</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("<p>This is a page generated from a servlet.</p>");
-		out.println("</body>");
-		out.println("</html>");
+		this.getServletContext().getRequestDispatcher("/WEB-INF/test.jsp").forward(req, resp);
 	}
 
 }
